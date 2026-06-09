a429592cc30c64d143b1754031845d52cadd7f8e
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-06-09 19:57:52 +0530

    sqlx to gORM in config file

--- config/config.go
+++ config/config.go
@@ -5,28 +5,27 @@ import (
 	"log"
 	"os"
 
-	"github.com/jmoiron/sqlx"
-	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
+    "github.com/joho/godotenv"
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
 )
 
-func ConnectDB() *sqlx.DB {
+func ConnectDB() *gorm.DB {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
-	// Print non-sensitive loaded env values; do not print raw .env or secrets.
+	
 	maskedPassword := "<redacted>"
 	maskedJWT := "<redacted>"
 	log.Printf("DB envs: DB_HOST=%q DB_PORT=%q DB_USER=%q DB_NAME=%q DB_PASSWORD=%q JWT_SECRET=%q\n",
 		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), maskedPassword, maskedJWT)
 
-	// Allow overriding connection via DATABASE_URL (e.g. postgres://user:pass@host:port/db)
 	databaseURL := os.Getenv("DATABASE_URL")
-	var db *sqlx.DB
+	var db *gorm.DB
 	if databaseURL != "" {
-		db, err = sqlx.Open("postgres", databaseURL)
+		db, err = gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 		if err != nil {
 			log.Fatal("Error connecting to database via DATABASE_URL: ", err)
 		}
@@ -40,7 +39,7 @@ func ConnectDB() *sqlx.DB {
 			os.Getenv("DB_NAME"),
 		)
 
-		db, err = sqlx.Open("postgres", connStr)
+		db, err = gorm.Open(postgres.Open(connStr), &gorm.Config{})
 		if err != nil {
 			log.Fatal("Error connecting to database: ", err)
 		}
--- go.mod
+++ go.mod
@@ -19,6 +19,12 @@ require (
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -37,7 +43,10 @@ require (
 	golang.org/x/arch v0.22.0 // indirect
 	golang.org/x/crypto v0.52.0 // indirect
 	golang.org/x/net v0.54.0 // indirect
+	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
+	gorm.io/gorm v1.31.1 // indirect
 )
