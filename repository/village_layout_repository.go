e6035f868f36fffb84bce71c6f320730b69c5c53
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-06-10 10:29:09 +0530

    sqlx to gORM in village_layout_repository

--- repository/village_layout_repository.go
+++ repository/village_layout_repository.go
@@ -1,15 +1,15 @@
 package repository
 
 import (
-	"github.com/jmoiron/sqlx"
+	"gorm.io/gorm"
 	"github.com/prashsti29/brave/models"
 )
 
 type VillageLayoutRepository struct {
-	database *sqlx.DB
+	database *gorm.DB
 }
 
-func NewVillageLayoutRepository(connection *sqlx.DB) *VillageLayoutRepository {
+func NewVillageLayoutRepository(connection *gorm.DB) *VillageLayoutRepository {
 	var villageRepo VillageLayoutRepository
 	villageRepo.database = connection
 	var result *VillageLayoutRepository
@@ -18,16 +18,14 @@ func NewVillageLayoutRepository(connection *sqlx.DB) *VillageLayoutRepository {
 }
 
 func (villageRepo *VillageLayoutRepository) CreateVillageLayout(layout *models.VillageLayout) error {
-	var err error
-	query := "INSERT INTO village_layout (player_id, building_id, x, y) VALUES ($1, $2, $3, $4)"
-	_, err = villageRepo.database.Exec(query, layout.PlayerID, layout.BuildingID, layout.X, layout.Y)
-	return err
+	var result *gorm.DB
+	result = villageRepo.database.Create(layout)
+	return result.Error
 }
 
 func (villageRepo *VillageLayoutRepository) GetVillageByPlayerID(playerID string) ([]models.VillageLayout, error) {
-	var err error
-	var layouts []models.VillageLayout
-	query := "SELECT * FROM village_layout WHERE player_id = $1"
-	err = villageRepo.database.Select(&layouts, query, playerID)
-	return layouts, err
+	 var layouts []models.VillageLayout
+    var result *gorm.DB
+    result = villageRepo.database.Where("player_id = ?", playerID).Find(&layouts)
+    return layouts, result.Error
 }
