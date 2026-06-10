0d1ed16a2a064b33eda6d3df076f3999505edff9
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-06-10 10:26:27 +0530

    sqlx to gORM in player_repository

--- repository/player_repository.go
+++ repository/player_repository.go
@@ -1,15 +1,15 @@
 package repository
 
 import (
-	"github.com/jmoiron/sqlx"
+	"gorm.io/gorm"
 	"github.com/prashsti29/brave/models"
 )
 
 type PlayerRepository struct {
-	database *sqlx.DB
+	database *gorm.DB
 }
 
-func NewPlayerRepository(connection *sqlx.DB) *PlayerRepository {
+func NewPlayerRepository(connection *gorm.DB) *PlayerRepository {
 	var playerRepo PlayerRepository
 	playerRepo.database = connection
 	var result *PlayerRepository
@@ -18,18 +18,16 @@ func NewPlayerRepository(connection *sqlx.DB) *PlayerRepository {
 }
 
 func (playerRepo *PlayerRepository) CreatePlayer(player *models.Player) error {
-	var err error
-	query := "INSERT INTO players (id, email, password_hash, dunbroch_level, gems, wisps, embis) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-	_, err = playerRepo.database.Exec(query, player.ID, player.Email, player.PasswordHash, player.DunbrochLevel, player.Gems, player.Wisps, player.Embis)
-	return err
+	 var result *gorm.DB
+    result = playerRepo.database.Create(player)
+    return result.Error
 }
 
 func (playerRepo *PlayerRepository) GetPlayerByID(id string) (*models.Player, error) {
-	var err error
 	var player models.Player
-	query := "SELECT * FROM players WHERE id = $1"
-	err = playerRepo.database.Get(&player, query, id)
-	var result *models.Player
-	result = &player
-	return result, err
+    var result *gorm.DB
+    result = playerRepo.database.First(&player, "id = ?", id)
+    var playerResult *models.Player
+    playerResult = &player
+    return playerResult, result.Error
 }
