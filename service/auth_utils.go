31d924316b780178bfe38c851825c866f3b3d3b0
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-01 00:55:59 +0530

    [auth] add password comparison helper

--- service/auth_utils.go
+++ service/auth_utils.go
@@ -8,3 +8,7 @@ func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
 }
+
+func comparePassword(password, hash string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
