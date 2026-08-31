541895e830541f8860409389c81006385be60c1b
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-01 00:55:54 +0530

    [auth] add auth utils file skeleton

---
+++ service/auth_utils.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+"golang.org/x/crypto/bcrypt"
+)
+
+func hashPassword(password string) (string, error) {
+bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+return string(bytes), err
+}
+
+func comparePassword(password, hash string) error {
+return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
