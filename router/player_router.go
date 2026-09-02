8dbcbe7a8b8b8e01e4e909fea946a1915ab0ee08
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-03 03:06:19 +0530

    [router] add register and login routes

--- router/player_router.go
+++ router/player_router.go
@@ -1,11 +1,19 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/prashsti29/brave/controllers"
+	"github.com/prashsti29/brave/middleware"
 )
 
 func RegisterPlayerRoutes(router *mux.Router, playerController *controllers.PlayerController) {
-	router.HandleFunc("/players", playerController.CreatePlayer).Methods("POST")
+	router.HandleFunc("/auth/register", playerController.Register).Methods("POST")
+	router.HandleFunc("/auth/login", playerController.Login).Methods("POST")
+	
+	router.Handle("/players", middleware.AuthMiddleware(http.HandlerFunc(playerController.CreatePlayer))).Methods("POST")
 	router.HandleFunc("/players/{id}", playerController.GetPlayer).Methods("GET")
+	
+	router.Handle("/profile", middleware.AuthMiddleware(http.HandlerFunc(playerController.GetProfile))).Methods("GET")
 }
