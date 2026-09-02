f7aa584a40adb49bc42e70ec15cf1109f3bbac15
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-03 05:27:53 +0530

    [db] create migration for buildings_configs table

--- db/migrations/000011_create_building_configs.up.sql
+++ db/migrations/000011_create_building_configs.up.sql
@@ -5,11 +5,9 @@ CREATE TABLE building_configs (
     upgrade_price INT NOT NULL,
     upgrade_time INT NOT NULL,
     currency VARCHAR(50) NOT NULL,
-    is_upgrading BOOLEAN NOT NULL,
-    upgrade_end_time TIMESTAMP NOT NULL,
+    max_health INT NOT NULL,
     dunbroch_level INT NOT NULL,
     max_allowed INT NOT NULL,
-    max_health INT NOT NULL,
     UNIQUE(name, level)
 );
 CREATE INDEX idx_building_configs_name ON building_configs(name);
