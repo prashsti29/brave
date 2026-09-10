add5821a8c1e6bc07204da87d9a6896948e1a7b8
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-11 00:43:33 +0530

    [router] add POST route for building creation

--- router/building_router.go
+++ router/building_router.go
@@ -1,13 +1,12 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/prashsti29/brave/controllers"
 )
 
 func RegisterBuildingRoutes(router *mux.Router, buildingController *controllers.BuildingController) {
 	router.HandleFunc("/buildings/{player_id}", buildingController.GetBuildingsByPlayerID).Methods("GET")
+	router.HandleFunc("/buildings/{player_id}", buildingController.AddBuilding).Methods("POST")
 	router.HandleFunc("/buildings/{player_id}/troops", buildingController.CreateTroops).Methods("POST")
 }
