4e19fe574373187c3703b863842c7b8a66eb17ed
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-12 03:32:38 +0530

    [controller] add UpgradeBuilding handler to BuildingController

--- internal/controllers/building_controller.go
+++ internal/controllers/building_controller.go
@@ -111,3 +111,18 @@ func (buildingController *BuildingController) AddBuilding(responseWriter http.Re
 	responseWriter.WriteHeader(http.StatusCreated)
 	json.NewEncoder(responseWriter).Encode(building)
 }
+
+// UpgradeBuilding handles building upgrade requests
+func (buildingController *BuildingController) UpgradeBuilding(responseWriter http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	buildingID := vars["building_id"]
+
+	building, err := buildingController.buildingService.UpgradeBuilding(buildingID)
+	if err != nil {
+		http.Error(responseWriter, "Could not upgrade building", http.StatusInternalServerError)
+		return
+	}
+
+	responseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(responseWriter).Encode(building)
+}
