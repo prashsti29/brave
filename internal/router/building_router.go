f3805c0d9d9fee0618ae160ffcb50da03bfcede7
prashsti29 <prashsti_sa@ce.iitr.ac.in>
2026-09-17 20:35:55 +0530

    [router] add PUT route for moving building

--- internal/router/building_router.go
+++ internal/router/building_router.go
@@ -11,4 +11,5 @@ func RegisterBuildingRoutes(router *mux.Router, buildingController *controllers.
 	router.HandleFunc("/buildings/{player_id}/troops", buildingController.CreateTroops).Methods("POST")
 	router.HandleFunc("/buildings/{building_id}/upgrade", buildingController.UpgradeBuilding).Methods("PUT")
 	router.HandleFunc("/buildings/{building_id}/complete", buildingController.CompleteUpgrade).Methods("POST")
+	router.HandleFunc("/buildings/{player_id}/{building_id}/move", buildingController.MoveBuilding).Methods("PUT")
 }
